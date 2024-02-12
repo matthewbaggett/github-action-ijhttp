89d18cd9cec7282770c5d9be891e0efbbb9729d4
Matthew Baggett <matthew@baggett.me>
2024-02-12 15:03:07 +0100

    Add support for passing envs and secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-set -e
+set -xe
+# For each new line seperated entry in $VARIABLES, prefix it with --env-variables=
+VARIABLES=$(echo "$VARIABLES" | sed -e 's| |\n|g' -e '/^$/d' -e 's|^|--env-variables=|g')
+# Same for SECRETS with --private-env-variables
+SECRETS=$(echo "$SECRETS" | sed -e 's| |\n|g' -e '/^$/d' -e 's|^|--private-env-variables=|g')
 
-/opt/ijhttp/ijhttp ${ENVIRONMENTS_FILE:+--env-file=${ENVIRONMENTS_FILE}} ${SELECTED_ENVIRONMENT:+--env=${SELECTED_ENVIRONMENT}} "${HTTP_FILE}"
+# shellcheck disable=SC2086
+/opt/ijhttp/ijhttp ${ENVIRONMENTS_FILE:+--env-file=${ENVIRONMENTS_FILE}} \
+	${SELECTED_ENVIRONMENT:+--env=${SELECTED_ENVIRONMENT}} \
+	${VARIABLES} \
+	${SECRETS} \
+	${HTTP_FILE}
